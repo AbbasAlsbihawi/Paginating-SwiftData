52a370b2a0f2483cc7ab38151a9c080f4d45f730
Abbas Alsbihawi <43404498+AbbasAlsbihawi@users.noreply.github.com>
2024-11-01 20:17:59 +0300

    init app

---
+++ Paginating SwiftData/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Paginating SwiftData
+//
+//  Created by Abbas on 08/03/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
